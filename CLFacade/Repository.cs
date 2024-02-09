e01f086b5d51ce4f0670d8c27eea65b80fe483e0
Simon Jensen <dkjsimon@gmail.com>
2024-02-09 11:58:14 +0100

    har forbedret farve generationen af hvert image, men det tager stadig for langt tid, plz optimizi

--- CLFacade/Repository.cs
+++ CLFacade/Repository.cs
@@ -3,15 +3,22 @@ using Models.MovieModels;
 using Microsoft.Extensions.Configuration;
 using MyModels;
 using System.Drawing;
+using AForge.Imaging.Filters;
+using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
 
 namespace CLFacade
 {
     public class Repository : IRepository
     {
         private DB db;
+        private List<string> listOfDominatColors { get; set; }
         public Repository(IConfiguration configuration)
         {
             db = new DB(configuration);
+            listOfDominatColors = new();
         }
         public async Task<Rootobject> GetMovieAsync()
         {
@@ -50,7 +57,8 @@ namespace CLFacade
 
                                                     Bitmap bitmap = new Bitmap(memoryStream);
 
-                                                    uiMovie.PosterDominantColor = ColorTranslator.ToHtml(await GetMostUsedColor(bitmap)).ToString();
+                                                    uiMovie.PosterDominantColor = ColorTranslator.ToHtml(await GetDominantColor(bitmap)).ToString();
+                                                    listOfDominatColors.Add(uiMovie.PosterDominantColor);
                                                 }
                                             }
                                         }
@@ -79,19 +87,41 @@ namespace CLFacade
             }
             return null;
         }
-        private async Task<Color> GetMostUsedColor(Bitmap bitMap)
+        private async Task<Color> GetDominantColor(Bitmap image)
         {
-            var colorIncidence = new Dictionary<int, int>();
-            for (var x = 0; x < bitMap.Size.Width; x++)
-                for (var y = 0; y < bitMap.Size.Height; y++)
+            // Convert the image to a list of colors
+            Color[] pixels = await GetMostUsedColor(image);
+
+            double[][] data = pixels.Select(c => new double[] { c.R, c.G, c.B }).ToArray();
+            KMeans kmeans = new KMeans(k: 5); 
+            var clusters = kmeans.Learn(data);
+            int[] largestCluster = clusters.Decide(data);
+            var clusterCounts = largestCluster.GroupBy(x => x).Select(g => new { Value = g.Key, Count = g.Count() });
+            var dominantCluster = clusterCounts.OrderByDescending(x => x.Count).First().Value;
+            var centroid = clusters.Clusters[dominantCluster].Centroid;
+            var dominantColor = Color.FromArgb((int)centroid[0], (int)centroid[1], (int)centroid[2]);
+
+            return dominantColor;
+        }
+        private async Task<Color[]> GetMostUsedColor(Bitmap bitMap)
+        {
+            ResizeBicubic filter = new ResizeBicubic(50, 50);
+            Bitmap resizedImage = filter.Apply(bitMap);
+            Color[] pixels = new Color[resizedImage.Width * resizedImage.Height];
+            int index = 0;
+
+            // Iterate through each pixel in the image
+            for (int x = 0; x < resizedImage.Width; x++)
+            {
+                for (int y = 0; y < resizedImage.Height; y++)
                 {
-                    var pixelColor = bitMap.GetPixel(x, y).ToArgb();
-                    if (colorIncidence.Keys.Contains(pixelColor))
-                        colorIncidence[pixelColor]++;
-                    else
-                        colorIncidence.Add(pixelColor, 1);
+                    // Get the color of the current pixel
+                    Color pixelColor = resizedImage.GetPixel(x, y);
+                    pixels[index++] = pixelColor;
                 }
-            return Color.FromArgb(colorIncidence.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value).First().Key);
+            }
+
+            return pixels;
         }
     }
 }
